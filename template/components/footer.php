4e4dd66ac8cd09307488184a84ca71150e95150d
kevinqi34 <sqi@novischools.net>
2017-01-12 19:35:47 -0500

    update

--- template/components/footer.php
+++ template/components/footer.php
@@ -5,4 +5,9 @@
 		<?php add_Citation($bool); ?>
 		<p>Built by <a href="http://www.kqi18.com">KQI</a></p>
     <p><a href="<?php echo url(); ?>other/privacypolicy.php">Privacy Policy</a></p>
+		<br>
 </div>
+
+
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-569d8e1db2d98f25"></script>
