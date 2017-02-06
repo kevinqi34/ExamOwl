46c3686291766ef1bd3ab8e9a7e5113810084533
kevinqi34 <sqi@novischools.net>
2017-02-05 20:10:22 -0500

    update

--- data/forum/post_header.php
+++ data/forum/post_header.php
@@ -7,7 +7,7 @@ $date = time_elapsed_string($id["CREATE_DATE"]);
 <div class="post_header">
     <h2><a href="<?php echo url(); ?>category/<?php echo $this->cat_id; ?>/<?php echo $this->cat_slug; ?>"><img src="<?php echo url(); ?>images/menu/return.png" alt="return" class="return"  /></a><?php echo $id['TITLE']; ?></h2>
     <p># of Responses: <?php echo $id['NUM_OF_COMMENTS']; ?></p>
-    <p>Posted <?php echo $date; ?> by <?php echo $id["AUTHOR"]; ?></p>
+    <p>Posted <?php echo $date; ?> by <a href="<?php echo url(); ?>/user/<?php echo $id["USER_ID"]; ?>"><?php echo $id["AUTHOR"]; ?></a></p>
 </div>
 <?php if ($id["CONTENT"]) { ?>
 <!-- If Content -->
