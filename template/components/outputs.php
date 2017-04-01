c60bb2dba93a301042cb8883e895b7491594090d
kevinqi34 <sqi@novischools.net>
2017-04-01 19:05:34 -0400

    update

--- template/components/outputs.php
+++ template/components/outputs.php
@@ -3,6 +3,6 @@
 <div id="output">
 
 <!-- Exit Icon -->
-<span id="output_exit"><img src="<?php echo url(); ?>/images/icons/close.png" /></span>
+<span id="output_exit"><img src="<?php echo url(); ?>images/icons/close.png" alt="close"/></span>
 <div id="output_content"></div>
 </div>
