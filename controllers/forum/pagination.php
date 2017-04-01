7594523be5780678211af5dd4d95f2604e48a394
kevinqi34 <sqi@novischools.net>
2017-04-01 17:57:20 -0400

    update

--- controllers/forum/pagination.php
+++ controllers/forum/pagination.php
@@ -14,7 +14,7 @@ if ($type == "comment") {
 } else if ($type == "thread") {
   $results = new thread($id);
   $results->delete_threads();
-  echo $results->privelege;
+  echo "hello";
   $results->pagination($page_number, $size);
 
 } else {
