96d66aec39f7a6a905200a57a29f767b6d67cd2a
kevinqi34 <sqi@novischools.net>
2017-03-31 23:30:58 -0400

    update

---
+++ controllers/user/user_records.php
@@ -0,0 +1,8 @@
+<?php
+  include ($_SERVER['DOCUMENT_ROOT'] . '/template/user/user.php');
+  echo "hello world";  
+
+
+
+
+?>
--- data/user/user_records.php
+++ data/user/user_records.php
@@ -1,1 +1,1 @@
-Hello World;
+<?php include ($_SERVER['DOCUMENT_ROOT'] . '/controllers/user/user_records.php')?>
