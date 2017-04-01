1a46fbdb2aad08399e2f597b6e2c943d2c29c859
kevinqi34 <sqi@novischools.net>
2017-03-31 23:29:12 -0400

    update

--- user_records.php
+++ user_records.php
@@ -4,7 +4,7 @@
   $title = "Exam Owl | User Records";
   $keywords = "Study for Exams, Exam Community, Exam Prep, College Forums, High School Forums, Exam Owl, Owl Exam, ExamOwl, Test Prep";
   $description = "Exam Owl is an online community that helps students ace their exams. Featuring a community of like-minded individuals and a suite of test prep tools and resources.";
-  $content = "/data/user_records.php";
+  $content = "/data/user/user_records.php";
   $css = ['css/admin/admin.css','css/forum/category.css', 'css/forum/thread.css', 'css/forum/post.css'];
   $js = [];
   // Instantiate template
