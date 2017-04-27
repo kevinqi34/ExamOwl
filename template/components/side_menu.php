a17e7436603c81312b268859583e9ea6177d1538
kevinqi34 <sqi@novischools.net>
2017-04-27 00:06:21 -0400

    update

--- template/components/side_menu.php
+++ template/components/side_menu.php
@@ -6,6 +6,17 @@
   <a href="<?php echo url(); ?>tools.php">Tools<img src="<?php echo url(); ?>images/icons/tools.png" alt="tools"/></a>
   <a href="<?php echo url(); ?>resources.php">Resources<img src="<?php echo url(); ?>images/icons/wiki.png" alt="resources"/></a>
 
-
+  <!-- Ad Block left -->
+  <div class="ad_block">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- Left Side -->
+      <ins class="adsbygoogle"
+           style="display:inline-block;width:160px;height:600px"
+           data-ad-client="ca-pub-9947718741109223"
+           data-ad-slot="6935293194"></ins>
+      <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+  </div>
 
 </div>
